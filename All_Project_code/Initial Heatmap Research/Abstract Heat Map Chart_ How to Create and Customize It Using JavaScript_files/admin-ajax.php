c4fca003d8e57870bc880b77ceffa32d89557118
Matthew Su <masu1223@colorado.edu>
2022-02-18 20:43:59 -0700

    Added inital heat-map research outcomes for team members to review when possible - Matthew

---
+++ All_Project_code/Initial Heatmap Research/Abstract Heat Map Chart_ How to Create and Customize It Using JavaScript_files/admin-ajax.php
@@ -0,0 +1,17 @@
+if (window.addthis_product === undefined) { window.addthis_product = "wpp"; } if (window.wp_product_version === undefined) { window.wp_product_version = "wpp-6.2.6"; } if (window.addthis_share === undefined) { window.addthis_share = {"passthrough":{"twitter":{"via":"anychart"}}}; } if (window.addthis_config === undefined) { window.addthis_config = {"data_track_clickback":false,"data_ga_property":"UA-228820-2","data_ga_social":true,"ui_atversion":300}; } if (window.addthis_plugin_info === undefined) { window.addthis_plugin_info = {"info_status":"enabled","cms_name":"WordPress","plugin_name":"Share Buttons by AddThis","plugin_version":"6.2.6","plugin_mode":"AddThis","anonymous_profile_id":"wp-a573240c79a04a3239707cb6c5eabb7a","page_info":{"template":false}}; } 
+                    (function() {
+                      var first_load_interval_id = setInterval(function () {
+                        if (typeof window.addthis !== 'undefined') {
+                          window.clearInterval(first_load_interval_id);
+                          if (typeof window.addthis_layers !== 'undefined' && Object.getOwnPropertyNames(window.addthis_layers).length > 0) {
+                            window.addthis.layers(window.addthis_layers);
+                          }
+                          if (Array.isArray(window.addthis_layers_tools)) {
+                            for (i = 0; i < window.addthis_layers_tools.length; i++) {
+                              window.addthis.layers(window.addthis_layers_tools[i]);
+                            }
+                          }
+                        }
+                     },1000)
+                    }());
+                
