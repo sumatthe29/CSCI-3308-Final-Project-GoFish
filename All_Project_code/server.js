ef319ad2c2838272cffd04f73f7b365e34cb20e1
Spencer Jackson <spja5809@colorado.edu>
2022-03-16 13:50:40 -0600

    Added line to server to let app read static files

--- All_Project_code/server.js
+++ All_Project_code/server.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser'); //Ensure our body-parser tool has been added
 app.use(bodyParser.json());    
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(express.static(__dirname + '/NodeJS')); //lets Node use static files like cs for styling - Spencer
 var numStudents = 0;
 var numAssignments = 0;
 //Create Database Connection
