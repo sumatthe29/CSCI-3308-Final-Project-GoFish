24dc41944f20b396094d0f6b1fda562dca26739d
YuheZou <yuzo5210@colorado.edu>
2022-03-28 17:14:06 -0600

    changed SQL file

--- All_Project_code/database/initialization.sql
+++ All_Project_code/database/initialization.sql
@@ -1,20 +1,47 @@
 DROP TABLE IF EXISTS Users;
 
 CREATE TABLE IF NOT EXISTS Users(          -- This table is for register page
-  User_id INT PRIMARY KEY,
+  User_Id INT NOT NULL,
   First_Name VARCHAR(45) NOT NULL,
   Last_Name VARCHAR(45) NOT NULL,
   User_Name VARCHAR(45) NOT NULL,
   User_Email VARCHAR(45) NOT NULL,		
   User_Password VARCHAR(45) NOT NULL,
-  User_Handle TEXT NOT NULL
+  User_Handle TEXT NOT NULL,
+  PRIMARY KEY(User_Id)
+);
+
+DROP TABLE IF EXISTS User_relationship(                 --User Buddies
+
+CREATE TABLE IF NOT EXISTS User_relationship(
+  User_Requester_Id INT NOT NULL,
+  User_Addressee_Id INT NOT NULL,
+  Created_Date DATE NOT NULL,
+  PRIMARY KEY(User_Requester_Id, User_Addressee_Id)
 );
 
 DROP TABLE IF EXISTS Posts;
 
 CREATE TABLE IF NOT EXISTS Posts(            -- This table is for Feed page
-  Post_id INT PRIMARY KEY,
+  Post_Id INT NOT NULL,
   Post_Name VARCHAR(45) NOT NULL,
   Post_Date DATE NOT NULL,
-  Post_Time TIME NOT NULL
+  Post_Time TIME NOT NULL, 
+  Post_Content TEXT NOT NULL,
+  User_id INT,
+  PRIMARY KEY(Post_Id),
+  FOREIGN KEY(User_Id) REFERENCES Users(User_Id)
+);
+
+DROP TABLE IF EXISTS Catches;
+
+CREATE TABLE IF NOT EXISTS Catches(                           --This table is for Catches
+  Catch_Id INT NOT NULL,
+  Catch_Name VARCHAR(45) NOT NULL,
+  Catch_Length FLOAT NOT NULL,
+  Catch_Loaction TEXT NOT NULL,
+  Catch_Date DATE NOT NULL,
+  User_id INT,
+  PRIMARY KEY(Catch_Id),
+  FOREIGN KEY(User_Id) REFERENCES Users(User_Id)
 );
