389ce60268aa203356981a90b5cb65d7f95f4721
Spencer Jackson <spja5809@colorado.edu>
2022-03-29 17:26:02 -0600

    Made first attempt at setup for docker-compose test enviornment

--- All_Project_code/server.js
+++ All_Project_code/server.js
@@ -1,11 +1,10 @@
 var express = require('express'); //Ensure our express framework has been added
 var app = express();
 var bodyParser = require('body-parser'); //Ensure our body-parser tool has been added
+var dotenv = require('dotenv').config(); //Lets us pull environement variables from .env for db configuration -- Spencer
 app.use(bodyParser.json());    
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-app.use(express.static(__dirname + '/NodeJS')); //lets Node use static files like cs for styling - Spencer
-var numStudents = 0;
-var numAssignments = 0;
+//app.use(express.static(__dirname + '/NodeJS')); lets Node use static files like cs for styling - Spencer
 //Create Database Connection
 var pgp = require('pg-promise')();
 
@@ -14,9 +13,9 @@ var pgp = require('pg-promise')();
 const dbConfig = {
 	host: 'db',
 	port: 5432,
-	database: 'football_db',
-	user: 'postgres',
-	password: 'pwd'
+	database: process.env.POSTGRES_DB, //process.env gets environement variables from .env, we reference them like this -- Spencer
+	user: process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD
 };
 
 var db = pgp(dbConfig);
@@ -27,7 +26,11 @@ app.set('view engine', 'ejs');
 //and access our resources directory
 app.use(express.static(__dirname + '/'));
 
+
+
+
 // initial get for 2feed2 page - Matthew
+
 
 app.get('/2feed2', function(res,req){
     var query = 'select * from posts;';
@@ -96,3 +99,34 @@ app.post('/2newpost2/create_post', function (req,res) {
     });
 });
 
+//Post function to process login request
+/*
+app.post('/login', function(req, res){
+    var username = req.body.uname;
+    var email = req.body.email;
+    var pword = req.body.pword;
+    var user;
+
+    if (email != "") {
+        user = `SELECT * FROM users WHERE User_Email = '${email}' AND User_Password = '${pword}';`;
+    } else {
+        user = `SELECT * FROM users WHERE User_Name = '${username}' AND User_Password = '${pword}';`;
+    }
+
+   db.any(user)
+    .then(function(rows){
+            res.render('pages/userprofile', {
+
+            })
+
+   })
+   .catch(function(err) {
+
+   })
+   
+});
+*/
+
+//Taken from lab 7, keeps server and front end connected
+app.listen(3000);
+console.log('3000 is the magic port');
