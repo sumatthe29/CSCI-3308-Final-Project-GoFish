da18d27f5b7c3bf6c18ff16eb5e37d094c13456f
VictoriaNawalany <vina5582@colorado.edu>
2022-04-06 12:08:47 -0600

    vina profile and catches tables added to database

--- All_Project_code/database/initialization.sql
+++ All_Project_code/database/initialization.sql
@@ -18,3 +18,28 @@ CREATE TABLE IF NOT EXISTS Posts(            -- This table is for Feed page
   Post_Date DATE NOT NULL,
   Post_Time TIME NOT NULL
 );
+
+DROP TABLE IF EXISTS Profile;                 -- This table is for Profile Page
+
+CREATE TABLE IF NOT EXISTS Profile(
+  User_id INT PRIMARY KEY, -- to link profile to user
+  User_Img -- need to save an image in her,
+  User_Friend_Handles -- need to save an array in here,
+  User_Bio TEXT,
+  Catch_1_Handle TEXT,
+  Catch_2_Handle TEXT,
+  Catch_3_Handle TEXT, -- these are to fetch the catches
+  Post_id INT -- to fetch most recent post
+)
+
+DROP TABLE IF EXISTS Catches;
+
+CREATE TABLE IF NOT EXISTS Catches(
+  Catch_id INT PRIMARY KEY,
+  Catch_Img -- img,
+  Catch_Species VARCHAR(45),
+  Catch_Length VARCHAR(5),
+  Catch_Weight VARCHAR(10),
+  Catch_Date DATE,
+  Catch_Location VARCHAR(45)
+)
